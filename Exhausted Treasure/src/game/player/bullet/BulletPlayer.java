063f4502e0d974d8e0f2736246080d61247e24d8
DNQ2101 <hoadd.2014@gmail.com>
2018-05-20 09:13:57 +0700

    init commit

---
+++ Exhausted Treasure/src/game/player/bullet/BulletPlayer.java
@@ -0,0 +1,58 @@
+package game.player.bullet;
+
+import base.GameObject;
+import base.Vector2D;
+import physic.BoxCollider;
+import physic.HitObject;
+import physic.PhysicBody;
+import physic.RunHitObject;
+import renderer.ImageRenderer;
+
+import java.awt.*;
+
+public class BulletPlayer extends GameObject implements PhysicBody, HitObject {
+
+    public Vector2D velocity;
+    public BoxCollider boxCollider;
+    private RunHitObject runHitObject;
+
+    // constructor
+    public BulletPlayer() {
+        this.velocity = new Vector2D();
+        this.renderer = new ImageRenderer("resources/images/Bullet/BulletEnerMy.png", 10, 10);
+        this.boxCollider = new BoxCollider(5, 5);
+        this.runHitObject = new RunHitObject(
+//                Enemy.class
+        );
+    }
+
+    @Override
+    public void run() {
+        super.run();
+        this.position.addUp(this.velocity);
+        this.boxCollider.position.set(this.position);
+        this.runHitObject.run(this);
+    }
+
+    @Override
+    public void getHit(GameObject gameObject) {
+        this.isAlive = false;
+    }
+
+
+    @Override
+    public BoxCollider getBoxCollider() {
+        return this.boxCollider;
+    }
+
+    public static class ShootSingle implements Shoot {
+    //    @Override
+    //    public void run(Playered playered) {
+    //        BulletPlayer bulletPlayer = GameObjectManager.instance.recycle(BulletPlayer.class);
+    //        bulletPlayer.position.set(playered.position);
+    //        Vector2D vector2D = new Vector2D(7, 0);
+    //        Vector2D rotate = vector2D.rotate(playered.playeredMove.angle);
+    //        bulletPlayer.velocity.set(rotate);
+    //    }
+    }
+}
